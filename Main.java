373b5014573607a862e46e8a6ba2521ff8bdf985
CarolusVMagnus <125180112+CarolusVMagnus@users.noreply.github.com>
2023-02-12 09:02:14 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,28 +1,47 @@
 package cuentas;
 
+/**
+ * Clase que representa calse principal.
+ * 
+ * @author carlo
+ * @version 1.0
+ */
+ 
 public class Main {
-
-    public static void operativa_cuenta(CCuenta cuenta1, double cantidad) {
+    public static void main(String[] args) {
+		/** Agrega un elemento al constructor CCuenta*/
+        CCuenta cuenta1;
         double saldoActual;
-        saldoActual = cuenta1.estado();
+
+        cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
+		/** Se opera con la cuenta1*/
+        operativa_cuenta(cuenta1, 0);
+    }
+
+    /**
+     * Realiza operaciones con una cuenta
+     * @param cuenta objeto de la clase CCuenta
+     * @param cantidad a ingresar o retirar
+     */
+	 
+    public static void operativa_cuenta(CCuenta cuenta, float cantidad) {
+        double saldoActual = cuenta.estado();
         System.out.println("El saldo actual es " + saldoActual);
 
         try {
-            cuenta1.retirar(cantidad);
+            System.out.println("Intentando retirar " + cantidad + "...");
+            cuenta.retirar(cantidad);
+            System.out.println("Retiro realizado con éxito");
         } catch (Exception e) {
             System.out.println("Fallo al retirar");
         }
 
         try {
-            System.out.println("Ingreso en cuenta");
-            cuenta1.ingresar(cantidad);
+            System.out.println("Intentando ingresar " + cantidad + "...");
+            cuenta.ingresar(cantidad);
+            System.out.println("Ingreso realizado con éxito");
         } catch (Exception e) {
             System.out.println("Fallo al ingresar");
         }
-    }
-
-    public static void main(String[] args) {
-        CCuenta cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
-        operativa_cuenta(cuenta1, 2300);
     }
 }
